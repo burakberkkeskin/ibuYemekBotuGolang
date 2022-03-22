94f3ee4927678865c16ae66e60fafdfa7a1f4124
safderun <burakberkkeskin@gmail.com>
2022-03-22 12:33:00 +0300

    fix

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
 func main() {
 
 	telegramHandler()
 
 }
 
-// func init() {
-// 	err := godotenv.Load(".envDev")
-// 	if err != nil {
-// 		log.Fatal("Error loading .env file")
-// 	}
-// }
+func init() {
+	err := godotenv.Load(".envDev")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
