bdeb3591f27574a67947f176fc9304f1eca0b823
safderun <burakberkkeskin@gmail.com>
2022-03-27 12:47:31 +0300

    fix

--- main.go
+++ main.go
@@ -2,22 +2,8 @@ package main
 
 import (
 	"ibuYemekBotu/services"
-	"log"
-	"os"
-
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	log.Println("Starting Bot")
 	services.TelegramHandler()
-	log.Println(os.Getenv("MONGODB_URI"))
-}
-
-func init() {
-	log.Println(os.Getenv("MONGODB_URI"))
-	err := godotenv.Load(".envDev")
-	if err != nil {
-		log.Println("Error loading .env file")
-	}
 }
